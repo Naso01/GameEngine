c22c484d2c77fc4a383f76d9f42214998793b504
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-01-22 12:19:00 -0500

    Singleton and  StandardIncludes added

---
+++ GameEngine/StandardIncludes.h
@@ -0,0 +1,32 @@
+#ifndef STANDARD_INCLUDES_H
+#define STANDARD_INCLUDES_H
+
+//Include standard headers
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cinttypes>
+#include <stdio.h>
+#include <thread>
+
+//Project headers
+#include "Singleton.h"
+
+//OS specific includes and defines
+#ifdef _WIN32
+//Windows specific
+	#include <Windows.h>
+	#include <direct.h>
+	#define M_ASSERT(_cond, _msg) \
+		if (!(_cond)) {OutputDebugStringA(_msg); std::abort();}
+	#define GetCurrentDir _getcwd
+#else
+//Linux specific
+	#include <unistd.h>
+	#define GetCurrentDir getcwd
+#endif
+
+using namespace std;
+
+
+#endif //STANDARD_INCLUDES_H
