b698d9b3dad7ce248f09189f2f5cd977ba92f187
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-01-29 17:42:11 -0500

    main is left to change

--- GameEngine/Resource.h
+++ GameEngine/Resource.h
@@ -2,7 +2,7 @@
 #define RESOURCE_H
 
 #include "Serializable.h"
-#include "ObjectPool.h"
+#include "AssetController.h"
 
 class Resource : public Serializable
 {
@@ -25,12 +25,15 @@ public:
 protected:
 	void SerializePointer(ostream& _stream, Resource* _pointer);
 	void DeserializePointer(istream& _stream, Resource*& _pointer);
+	void SerializeAsset(ostream& _stream, Asset* _asset);
+	void DeserializeAsset(istream& _stream, Asset*& _asset);
 
 private:
 	int m_val1;
 	double m_val2;
 	char m_val3;
 	Resource* m_subResource;
+	Asset* m_asset;
 };
 
 #endif // RESOURCE_H
