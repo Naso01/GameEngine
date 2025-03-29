f922b3e33b4e9572f55b3df921320537709594e8
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-03-28 21:20:19 -0400

    matrix Transformations: Rotate (example: y axis)

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -5,9 +5,11 @@ using namespace glm;
 
 int main() {
 	mat4 matrix = mat4(1.0f);
-	vec3 scaleVec = { 2, 2, 2 };
-	matrix = scale(matrix, scaleVec);
-	cout << "Scale Matrix: " << to_string(matrix) << endl;
+	float angle = 0.1f;
+	vec3 axis = { 0, 1, 0 };
+	matrix = rotate(matrix, angle, axis);
+	cout << "Rotation Matrix: " << to_string(matrix) << endl;
 
 	return 0;
 }
+
