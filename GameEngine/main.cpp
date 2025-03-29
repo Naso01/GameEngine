a8dcee89f75cfc11628d77f5ceda7c513482b8bd
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-03-28 21:18:57 -0400

    matrix Transformations: Scale

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -3,17 +3,11 @@
 using namespace glm;
 
 
-
 int main() {
-	mat4 matrix1 = mat4(1.0f);
-	vec3 pos1 = { 0, 0, 0 };
-	matrix1 = translate(matrix1, pos1);
-	cout << "Translation Matrix 1: " << to_string(matrix1) << endl;
-
-	mat4 matrix2 = mat4(1.0f);
-	vec3 pos2 = { 1, 2, 3 };
-	matrix2 = translate(matrix2, pos2);
-	cout << "Translation Matrix 2: " << to_string(matrix2) << endl;
+	mat4 matrix = mat4(1.0f);
+	vec3 scaleVec = { 2, 2, 2 };
+	matrix = scale(matrix, scaleVec);
+	cout << "Scale Matrix: " << to_string(matrix) << endl;
 
 	return 0;
 }
