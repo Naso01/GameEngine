0a1043170437520575b16d17f3988852d443a4e8
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-04-04 19:56:16 -0400

    .

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -1,26 +1,8 @@
 #include "GameController.h"
 
-using namespace glm;
-
-static void AddVectors() {
-
-	vec2 a = { 3, 5 };
-	vec2 b = { 2, -1 };
-	vec2 c = a + b;
-	cout << glm::to_string(c) << endl;
-
-	vec3 d = { 3, 5, 1};
-	vec3 e = { 2, -1, -1 };
-	vec3 f = d + e;
-	cout << glm::to_string(f) << endl;
-
-}
-
-
-int main(void)
+int main()
 {
-	AddVectors();
+	GameController::Instance().RunGame();
 
 	return 0;
 }
-
